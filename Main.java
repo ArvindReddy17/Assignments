5a62ae9794c6b620ae82ba87c7020ac852d49e75
Arvind <gokularvindkumar18@gmail.com>
2022-10-26 09:42:05 +0530

    First Assignment

---
+++ Main.java
@@ -0,0 +1,223 @@
+public class Main {
+        public static void main(String[] args) {
+            // TODO Auto-generated method stub
+
+            int n=7;
+            for(int i=0;i<n;i++)
+            {
+                for(int j=0;j<n;j++)
+                {
+
+                    if((i==0 && j!=0 && j!=n-1)||(j==0 && i!=0) || (j==n-1 && i!=0) || i==(n-1)/2  )
+                    {
+                        System.out.print("*");
+                    }
+                    else
+                    {
+                        System.out.print(" ");
+                    }
+
+                }
+                System.out.print("   ");
+                for(int j=0;j<n;j++)
+                {
+
+                    if(( j==0 && i!=0 ) || (j==(3*n)/4 && i!=0 && i!=(n-1)/2) || (i==0 && j!=0 && j<(3*n)/4) || (i==(n-1)/2 && j<(3*n)/4 && j!=1 ) )
+                    {
+                        System.out.print("*");
+                    }
+                    else
+                    {
+                        System.out.print(" ");
+                    }
+
+                }
+                System.out.print("   ");
+                for(int j=0;j<n;j++)
+                {
+
+                    if(i==j)
+                    {
+                        System.out.print("*");
+                    }
+                    else
+                    {
+                        System.out.print(" ");
+                    }
+
+                }
+                System.out.print("");
+                for(int j=0;j<n;j++)
+                {
+
+                    if(i+j==n-1)
+                    {
+                        System.out.print("*");
+                    }
+                    else
+                    {
+                        System.out.print(" ");
+                    }
+
+                }
+                System.out.print("   ");
+                for(int j=0;j<n;j++)
+                {
+
+                    if(i==0 || i==n-1 || j==(n-1)/2)
+                    {
+                        System.out.print("*");
+                    }
+                    else
+                    {
+                        System.out.print(" ");
+                    }
+
+                }		System.out.print("   ");
+                for(int j=0;j<n;j++)
+                {
+
+                    if(j==0 || j==n-1 || i==j )
+                    {
+                        System.out.print("*");
+                    }
+                    else
+                    {
+                        System.out.print(" ");
+                    }
+
+                }
+                System.out.print("   ");
+
+                for(int j=0;j<n;j++)
+                {
+
+                    if((j==0 && i!=n-1) || (j==n-1 && i!=0 && i!=n-1) || (i==0 && j!=0 && j!= n-1) || (i==n-1  && j!= n-1))
+                    {
+                        System.out.print("*");
+                    }
+                    else
+                    {
+                        System.out.print(" ");
+                    }
+
+                }	System.out.print("   ");
+
+
+
+                System.out.println();
+
+
+            }
+
+            System.out.println();
+            System.out.println();
+
+            for(int i=0;i<n;i++)
+            {
+                for(int j=0;j<n;j++)
+                {
+
+                    if(i==0 || i==n-1 || j==(n-1)/2)
+                    {
+                        System.out.print("*");
+                    }
+                    else
+                    {
+                        System.out.print(" ");
+                    }
+
+                }
+                System.out.print("   ");
+                for(int j=0;j<n;j++)
+                {
+
+                    if(j==0 || j==n-1 || i==j )
+                    {
+                        System.out.print("*");
+                    }
+                    else
+                    {
+                        System.out.print(" ");
+                    }
+
+                }
+                System.out.print("   ");
+                for(int j=0;j<n;j++)
+                {
+
+                    if((j==0) || i==0 || i==(n-1)/2 || i==n-1)
+                    {
+                        System.out.print("*");
+                    }
+                    else
+                    {
+                        System.out.print(" ");
+                    }
+
+                }
+                System.out.print("   ");
+                for(int j=0;j<n;j++)
+                {
+
+                    if((j==0 && i!= n-1) || (j==n-1 && i!=n-1) || (i==n-1 && j!=0 && j!=n-1))
+                    {
+                        System.out.print("*");
+                    }
+                    else
+                    {
+                        System.out.print(" ");
+                    }
+
+                }
+                System.out.print("   ");
+                for(int j=0;j<n;j++)
+                {
+
+                    if(( j==0 && i!=0 ) || (j==(3*n)/4 && i!=0 && i!=(n-1)/2) || (i==0 && j!=0 && j<(3*n)/4) || (i==(n-1)/2 && j<(3*n)/4 && j!=1 ) )
+                    {
+                        System.out.print("*");
+                    }
+                    else
+                    {
+                        System.out.print(" ");
+                    }
+
+                }
+                System.out.print("   ");
+                for(int j=0;j<n;j++)
+                {
+
+                    if((i==0 && j!=0 && j!=n-1) || (j==0 && i!=0 && i!=n-1) || (i==n-1 && j!=0 && j!= n-1) || (j==n-1 && i!=0 && i!=n-1))
+                    {
+                        System.out.print("*");
+                    }
+                    else
+                    {
+                        System.out.print(" ");
+                    }
+
+                }
+                System.out.print("   ");
+                for(int j=0;j<n;j++)
+                {
+
+                    if(j==0 || j==n-1 || i==j )
+                    {
+                        System.out.print("*");
+                    }
+                    else
+                    {
+                        System.out.print(" ");
+                    }
+
+                }
+
+
+
+                System.out.println();
+
+            }
+
+        }
+    }
